9548f9d6ca648e43ed47297116781aa52c5c9193
Vincent <90656714+VincentCadicamo@users.noreply.github.com>
2024-09-30 19:11:07 -0400

    Create testsaber5.py

---
+++ Fall_Prototype1/testsaber5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import serial
+
+ser = serial.Serial(
+    "/dev/serial0", # Serial port for UART (GPIO 14 is TX)
+    baudrate=9600, # Sabertooth's default baud rate
+    timeout=1
+)
+
+def motor_command(motor, speed):
+    if motor == 1:
+        command = speed + 64  # Map to 1â€“127 for motor 1
+        if command < 1:
+            command = 1
+        elif command > 127:
+            command = 127
+    elif motor == 2:
+        command = speed + 192  # Map to 128â€“255 for motor 2
+        if command < 128:
+            command = 128
+        elif command > 255:
+            command = 255
+    ser.write(bytes([speed]))
+
+
+while True:
+    x = int(input("Speed (-127 to 127): "))
+    motor_command(1, x)
+    motor_command(2, x)
